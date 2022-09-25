f5319dc8c1772b37c38a6ade3142c639dcbab4fa
Raquel Stankey <brv1114@yahoo.com>
2022-09-25 14:40:22 -0500

    pushing all updates

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(require("./routes"));
 
 //mongoose connection
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/social-network",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/social-network-api",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,

c63209fb983ddbc318332b7c34a4a3f150d4ffb1
Raquel Stankey <brv1114@yahoo.com>
2022-09-25 19:36:43 -0500

    pushing code

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,7 +1,5 @@
 const { Schema, Types } = require("mongoose");
 
-// This will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model.
-
 const reactionSchema = new Schema({
   reactionId: {
     type: Schema.Types.ObjectId,
@@ -10,7 +8,6 @@ const reactionSchema = new Schema({
   reactionBody: {
     type: String,
     required: true,
-    // 280 characters maximum
     maxlength: 280,
   },
   username: {
@@ -19,9 +16,7 @@ const reactionSchema = new Schema({
   },
   createdAt: {
     type: Date,
-    // Sets the default value to the current timestamp
     default: Date.now,
-    // Getter method to format the timestamp on query
     get: (date) => {
       if (date) {
         return date.toLocaleDateString();
